5194d04810a1d7c6d76de5664834333a817a7c3b
Grzegorz Zygmański <gzygmanski@hotmail.com>
2019-12-28 14:42:13 +0100

    dracula theme, patches, and minor changes: gaps to 0, added transparency

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -10,23 +10,17 @@ static const int horizpadbar        = 2;        /* horizontal padding for status
 static const int vertpadbar         = 2;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#44475a";
-static const char col_gray3[]       = "#f8f8f2";
-static const char col_gray4[]       = "#f8f8f2";
-static const char col_cyan[]        = "#8be9fd";
-static const unsigned int baralpha = 0xF5;
-static const unsigned int borderalpha = OPAQUE;
+static const char col_gray1[]       = "#fdf6e3";
+static const char col_gray2[]       = "#002b36";
+static const char col_gray3[]       = "#657b83";
+static const char col_gray4[]       = "#fdf6e3";
+static const char col_cyan[]        = "#657b83";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_gray2, col_cyan },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
-};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -36,9 +30,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      		instance    title       tags mask     isfloating   monitor */
-	{ "Steam",	  	NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "qutebrowser",  	NULL,       NULL,       1 << 1,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -68,29 +62,22 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "qutebrowser", NULL };
-static const char *ncmpcppcmd[] = { "st", "-e", "ncmpcpp", NULL };
 
 /* mpc commands */
-static const char *mpdtoggle[]	= { "mpc", "toggle", NULL };
-static const char *mpdstop[]	= { "mpc", "stop", NULL };
-static const char *mpdnext[]	= { "mpc", "next", NULL };
-static const char *mpdprev[]	= { "mpc", "prev", NULL };
-static const char *mpdvolp[]	= { "mpc", "volume", "+5", NULL };
-static const char *mpdvolm[]	= { "mpc", "volume", "-5", NULL };
-static const char *mpdseekp[]	= { "mpc", "seek", "+2%", NULL };
-static const char *mpdseekm[]	= { "mpc", "seek", "-2%", NULL };
+static const char *mpdtoggle[]	= { "mpc", "toggle", NULL};
+static const char *mpdstop[]	= { "mpc", "stop", NULL};
+static const char *mpdnext[]	= { "mpc", "next", NULL};
+static const char *mpdprev[]	= { "mpc", "prev", NULL};
+static const char *mpdvolp[]	= { "mpc", "volume", "+5", NULL};
+static const char *mpdvolm[]	= { "mpc", "volume", "-5", NULL};
+static const char *mpdseekp[]	= { "mpc", "seek", "+2%", NULL};
+static const char *mpdseekm[]	= { "mpc", "seek", "-2%", NULL};
 
-/* volume commands */
-static const char *volp[]	= { "pactl", "set-sink-volume", "0", "+2%", NULL };
-static const char *volm[]	= { "pactl", "set-sink-volume", "0", "-2%", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,	                XK_grave,  spawn,          {.v = browsercmd} },
-	{ MODKEY|ShiftMask,             XK_m, 	   spawn,          {.v = ncmpcppcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -133,11 +120,6 @@ static Key keys[] = {
 	{ 0,				XF86XK_AudioLowerVolume,	spawn,	{.v = mpdvolm } },
 	{ MODKEY,			XK_bracketright,		spawn,	{.v = mpdseekp } },
 	{ MODKEY,			XK_bracketleft,			spawn,	{.v = mpdseekm } },
-
-	/* volume controls */
-	{ MODKEY|ShiftMask,		XK_plus,			spawn,	{.v = volp } },
-	{ MODKEY|ShiftMask,		XK_underscore,			spawn,	{.v = volm } },
-
 };
 
 /* button definitions */
@@ -148,12 +130,12 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         ShiftMask,      Button1,        movemouse,      {0} },
-	{ ClkClientWin,         ShiftMask,      Button2,        togglefloating, {0} },
-	{ ClkClientWin,         ShiftMask,      Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            ShiftMask,      Button1,        tag,            {0} },
-	{ ClkTagBar,            ShiftMask,      Button3,        toggletag,      {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 

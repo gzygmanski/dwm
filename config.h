0db8c1d73b02641bb530135e8aa0d7dc4c52f778
Grzegorz Zygmański <gzygmanski@hotmail.com>
2019-12-29 20:14:22 +0100

    dmenu colors to match theme; volume shortcuts fix

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "qutebrowser", NULL };
 static const char *ncmpcppcmd[] = { "st", "-e", "ncmpcpp", NULL };
@@ -135,8 +135,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_bracketleft,			spawn,	{.v = mpdseekm } },
 
 	/* volume controls */
-	{ MODKEY|ShiftMask,		XK_plus,			spawn,	{.v = volp } },
-	{ MODKEY|ShiftMask,		XK_underscore,			spawn,	{.v = volm } },
+	{ MODKEY,		        XK_equal,			spawn,	{.v = volp } },
+	{ MODKEY,		        XK_minus,			spawn,	{.v = volm } },
 
 };
 
@@ -148,12 +148,12 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         ShiftMask,      Button1,        movemouse,      {0} },
-	{ ClkClientWin,         ShiftMask,      Button2,        togglefloating, {0} },
-	{ ClkClientWin,         ShiftMask,      Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask,      Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask,      Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY|ShiftMask,      Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            ShiftMask,      Button1,        tag,            {0} },
-	{ ClkTagBar,            ShiftMask,      Button3,        toggletag,      {0} },
+	{ ClkTagBar,            MODKEY|ShiftMask,      Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY|ShiftMask,      Button3,        toggletag,      {0} },
 };
 
